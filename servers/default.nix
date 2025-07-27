a38938a08cbd3fdd94014efafd8b4d2481ef2ac6
zbellpeppers <github@bell-peppers.com>
2025-07-27 08:35:58 -0400

    reenable containers + prayer

--- servers/default.nix
+++ servers/default.nix
@@ -1,11 +1,10 @@
 { ... }:
 {
   imports = [
-    #./budget
+    ./budget
     ./cloudflare
-    #./ddclient
-    #./headscale
-    #./nginx
+    ./headscale
+    ./nginx
     ./samba
   ];
   virtualisation.containers.enable = true;
